47e2d20fa48d11c855f4232c49362b78fa0b4838
ahsouza <annibalhsouza@gmail.com>
2021-03-20 16:33:35 -0300

    feat: generating new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+import { CardComponent } from './card/card.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
+import { InputComponent } from './form/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SectionComponent,
+    CardComponent,
+    AvatarComponent,
+    FormComponent,
+    TableComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
