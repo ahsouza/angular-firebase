80a6e84dc4232b57c58a72740936bf40884eb51c
ahsouza <annibalhsouza@gmail.com>
2021-03-20 16:54:41 -0300

    feat: implement material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AvatarComponent } from './avatar/avatar.component';
 import { FormComponent } from './form/form.component';
 import { TableComponent } from './table/table.component';
 import { InputComponent } from './form/input/input.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +25,9 @@ import { InputComponent } from './form/input/input.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
